74b889e02d0598183c70dd0ba187306c881248ea
Tanimul Haque Khan <tazimtazim2012@gmail.com>
2023-10-31 15:27:42 +0600

    ## [v0.0.1] - 2023-10-31
    Initial Release
    
    ### Added
    - SteamAchievementProvider Added
    - Samples Added
    - Added docgen.yml
    - Added Upmbranchupdate.yml

---
+++ Samples/AchievementsTable.cs
@@ -0,0 +1,10 @@
+namespace Studio23.SS2.AchievementSystem.Data
+{
+	public static class AchievementsTable
+	{
+		public static readonly string ACHIEVEMENT_NAME_1 = "ACHIEVEMENT_NAME_1";
+		public static readonly string ACHIEVEMENT_NAME_2 = "ACHIEVEMENT_NAME_2";
+		public static readonly string ACHIEVEMENT_NAME_3 = "ACHIEVEMENT_NAME_3";
+		public static readonly string ACHIEVEMENT_NAME_4 = "ACHIEVEMENT_NAME_4";
+	}
+}
