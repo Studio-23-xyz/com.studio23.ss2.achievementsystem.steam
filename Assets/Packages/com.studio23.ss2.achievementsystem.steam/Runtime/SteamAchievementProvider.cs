387f7896203881f089f056e3c57b6639d39c0b1a
dipokDipu <90243333+dipokDipu@users.noreply.github.com>
2025-05-12 14:02:26 +0600

    Fixed Steam Achievements

--- Assets/Packages/com.studio23.ss2.achievementsystem.steam/Runtime/SteamAchievementProvider.cs
+++ Assets/Packages/com.studio23.ss2.achievementsystem.steam/Runtime/SteamAchievementProvider.cs
@@ -1,24 +1,91 @@
+using Cysharp.Threading.Tasks;
 using Steamworks;
 using Steamworks.Data;
-using Studio23.SS2.AchievementSystem.Providers;
+using Studio23.SS2.AchievementSystem.Data;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
-public class SteamAchievementProvider : AchievementProvider
+
+[CreateAssetMenu(fileName = "Steam Achievement Provider", menuName = "Studio-23/AchievementSystem/Steam Provider", order = 1)]
+
+public class SteamAchievementProvider : AbstractAchievementProvider
 {
 
     public uint SteamAppID;
 
-    public override void Initialize()
+    public override UniTask<AchievementData> GetAchievement(string id)
     {
-        SteamClient.Init(SteamAppID);
+        string mappedID = _achievementMapper.IDMaps.Find(r => r.Key == id).Value;
+
+        var achievement = SteamUserStats.Achievements.FirstOrDefault(a => a.Identifier == mappedID);
+
+        if (string.IsNullOrEmpty(achievement.Identifier))
+        {
+            Debug.LogWarning($"Achievement with ID '{mappedID}' not found.");
+            return UniTask.FromResult<AchievementData>(null);
+        }
+
+        var data = new AchievementData
+        {
+            Id = id,
+            Name = achievement.Name,
+            IsUnlocked = achievement.State,
+            ProgressState = achievement.State ? "Unlocked" : "Locked",
+            Progression = achievement.State ? 1f : 0f,
+            UnlockedDescription = achievement.Description,
+            LockedDescription = achievement.Description
+        };
+
+        return UniTask.FromResult(data);
     }
 
-    public override void UnlockAchievement(string achievementIdentifier)
+    public override UniTask<AchievementData[]> GetAllAchievements()
     {
-        string mappedID= _mapper.IDMaps.Find(r=>r.Key == achievementIdentifier).Value;
+        List<AchievementData> list = new();
+
+        foreach (var map in _achievementMapper.IDMaps)
+        {
+            var achievement = SteamUserStats.Achievements.FirstOrDefault(a => a.Identifier == map.Value);
+
+            if (string.IsNullOrEmpty(achievement.Identifier))
+            {
+                Debug.LogWarning($"Achievement '{map.Value}' not found.");
+                continue;
+            }
+
+            var data = new AchievementData
+            {
+                Id = map.Key,
+                Name = achievement.Name,
+                IsUnlocked = achievement.State,
+                ProgressState = achievement.State ? "Unlocked" : "Locked",
+                Progression = achievement.State ? 1f : 0f,
+                UnlockedDescription = achievement.Description,
+                LockedDescription = achievement.Description
+            };
+
+            list.Add(data);
+        }
+        return UniTask.FromResult(list.ToArray());
+
+    }
+
+    public override UniTask<int> Initialize()
+    {
+        SteamClient.Init(SteamAppID);
+        return UniTask.FromResult((int)SteamAppID);
+    }
+
+    public override UniTask<int> UpdateAchievementProgress(string id, float progression)
+    {
+        string mappedID = _achievementMapper.IDMaps.Find(r => r.Key == id).Value;
         var achievement = new Achievement(mappedID);
         achievement.Trigger();
+        return new UniTask<int>(1);
     }
+
+
 
     [ContextMenu("Reset All Stats")]
     internal void ClearAllStats()
